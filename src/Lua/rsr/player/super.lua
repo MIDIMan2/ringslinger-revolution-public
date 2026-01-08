6c1de7906bcfd65c629a548a56ea96b844527e75
orbitalviolet <orbitalviolet999@gmail.com>
2026-01-08 13:17:22 +0000

    Undo Schrodinger's SuperBurnout

--- src/Lua/rsr/player/super.lua
+++ src/Lua/rsr/player/super.lua
@@ -52,7 +52,7 @@ RSR.PlayerSuperTick = function(player)
 	end
 
 	-- Don't let the player lose their emeralds when detransforming (unless rsr_superburnout is disabled)
-	if player.playerstate == PST_LIVE and not player.powers[pw_emeralds] and player.rsrinfo.lastemeralds and RSR.CV_SuperBurnout.value then
+	if player.playerstate == PST_LIVE and not player.powers[pw_emeralds] and player.rsrinfo.lastemeralds and not RSR.CV_SuperBurnout.value then
 		-- for spawner in mobjs.iterate() do
 		-- 	if not (Valid(spawner) and spawner.type == MT_EMERALDSPAWN) then return end
 		-- 	spawner.threshold = 0 -- Reset the counter so the emeralds don't spawn
